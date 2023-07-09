49cd91b27ef9cf88da9da7bf9cec882400a80d33
yuta10200831 <yfuse20ki@yahoo.co.jp>
2023-07-09 13:04:49 +0900

    課題1　提出

--- src/public/index.php
+++ src/public/index.php
@@ -1,2 +1,11 @@
-<?php
-echo 'Welcome TECH QUEST!';
+<?php 
+$countries = [
+    "ブラジル" => 100,
+    "日本" => 150,
+    "スペイン" => 50000
+];
+
+foreach ($countries as $name => $price) {
+  echo $name . ":" . $price . "人";
+  echo "<br>";
+}
