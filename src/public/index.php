7e7f98a929b25efd607a924a34e649881e2b5975
yuta10200831 <yfuse20ki@yahoo.co.jp>
2023-07-09 22:40:08 +0900

    6. 連想配列, 多次元配列、課題2の提出

--- src/public/index.php
+++ src/public/index.php
@@ -1,11 +1,25 @@
 <?php 
-$countries = [
-    "ブラジル" => 100,
-    "日本" => 150,
-    "スペイン" => 50000
-];
+//$countries = [
+    //"ブラジル" => 100,
+    //"日本" => 150,
+    //"スペイン" => 50000
+//];
 
-foreach ($countries as $name => $price) {
-  echo $name . ":" . $price . "人";
-  echo "<br>";
+//foreach ($countries as $name => $price) {
+  //echo $name . ":" . $price . "人";
+  //echo "<br>";
+//}
+
+$animals1 = ["いぬ" => 1, "ねこ" => 2, "うさぎ" => 6];
+$animals2 = ["ぞう" => 1, "きりん" => 1, "さい" => 4];
+$animalsList = ["小動物" => $animals1, "大動物" => $animals2];
+
+$myOwnAnimalList = [];
+foreach ($animalsList as $animals) {
+    foreach ($animals as $animalName => $animalNum) {
+        if ($animalNum > 1) {
+            $myOwnAnimalList[] = $animalName;
+        }
+    }
 }
+echo implode("<br>", $myOwnAnimalList);
